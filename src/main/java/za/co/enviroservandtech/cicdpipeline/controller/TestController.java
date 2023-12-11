6bfd4bc4c6a82ea6481b91fce1c1b37c591841a4
Masienyane Prince <prince.masienyane@gmail.com>
2023-12-11 22:06:30 +0200

    added build and docker file

--- src/main/java/za/co/enviroservandtech/cicdpipeline/controller/TestController.java
+++ src/main/java/za/co/enviroservandtech/cicdpipeline/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-
+//cicdpipeline-service-role
 @RestController
 @RequestMapping(path = "/test")
 public class TestController {
